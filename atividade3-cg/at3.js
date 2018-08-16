9850b9a6983ac45733ba34e48faa702052122287
mateusab <mateusabantunes@hotmail.com>
2018-08-16 16:44:06 -0300

    update das teclas de camera apenas

--- atividade3-cg/at3.js
+++ atividade3-cg/at3.js
@@ -1,5 +1,5 @@
 var container;
-var camera, controls, scene, renderer;
+var camera, camera1, controls, scene, renderer;
 var count = 0;  
 
 function init() {
@@ -40,6 +40,9 @@ function init() {
   /* Camera */
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 2000);
   camera.applyMatrix(cameraMatrix);
+
+  camera1 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 2000);
+  camera1.applyMatrix(cameraMatrix);
 
   camera2 = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera2.position.set(-120, 0, -200);
@@ -123,10 +126,10 @@ function init() {
   //light.position.set(4,4,6);
   //scene.add(light);
 
+  
   ronaldo = new THREE.Object3D();
   mtlLoader.load('images/ronaldo.mtl', function(materials){
     materials.preload();      
-    /* Carregando o Objeto */
     var objLoader = new THREE.OBJLoader();
     objLoader.setMaterials(materials);
     objLoader.load('images/ronaldo.obj', function(object){
@@ -140,21 +143,21 @@ function init() {
     });
   });
   scene.add(ronaldo);
-
+  
   /* The Rock */
   therock = new THREE.Object3D();
-  mtlLoader.load('images/Rock.mtl', function(materials){
+  mtlLoader.load('images/ronaldo_goal.mtl', function(materials){
     materials.preload();      
     /* Carregando o Objeto */
     var objLoader = new THREE.OBJLoader();
     objLoader.setMaterials(materials);
-    objLoader.load('images/Rock.obj', function(object){
+    objLoader.load('images/ronaldo.obj', function(object){
       //Seta o posicionamento do objeto
-      object.position.set(0, -30, -200);
+      object.position.set(70, -35, -200);
       object.scale.set(0.5, 0.5, 0.5);
-      therock.position.set(0,0,0);
+      therock.position.set(50,0,370);
       //O comando abaixo deixa o CR7 de Costa
-      therock.rotation.y += 500;
+      therock.rotation.y += -50;
       therock.add(object);
     });
   });
@@ -201,6 +204,14 @@ function init() {
     }
     if (keyCode == 39){
       therock.position.x += 5;
+    }
+
+    if (keyCode == 67){
+      camera = camera1;
+    }
+
+    if (keyCode == 86){
+      camera = camera2;
     }
   render();
   }
